c6cb858ce3fae07436bbd7a6c85b936611e0d142
ignacy <ignacy.buczek@onet.pl>
2016-11-30 15:56:33 +0100

    Added init.sql

---
+++ src/main/resources/db/init/init.sql
@@ -0,0 +1,188 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 30, 2016 at 03:51 PM
+-- Server version: 5.7.16-0ubuntu0.16.04.1
+-- PHP Version: 7.0.8-0ubuntu0.16.04.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `provoc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `Id` int(10) UNSIGNED NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `name` char(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`Id`, `teacher_id`, `name`) VALUES
+(1, 6, 'test'),
+(3, 6, 'test2'),
+(4, 7, 'marian\'s group');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invitation`
+--
+
+CREATE TABLE `invitation` (
+  `uid` varchar(60) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `groupId` int(10) UNSIGNED NOT NULL,
+  `studentName` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `invitation`
+--
+
+INSERT INTO `invitation` (`uid`, `email`, `groupId`, `studentName`) VALUES
+('9a47ae26-5f42-4416-8e1c-d6c0b4601c79', 'najusz@example.com', 1, 'Najusz');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `first_name` varchar(60) NOT NULL,
+  `last_name` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `email`, `password`, `first_name`, `last_name`) VALUES
+(1, 'janusz@example.com', '$2a$10$YPRvp9BtfeK.DmMdlEeRJegN2SyQsx4Ei.Qcwfa9l44QtRA.jre5y', 'Janusz', 'Kowalski'),
+(3, 'janusz2@example.com', '1234567Aa', 'Janusz', 'Kowalski'),
+(7, 'krzysztof@example.com', 'e23we2q3wevqfe', 'Krzystof', 'Nowak');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_group`
+--
+
+CREATE TABLE `student_group` (
+  `student_id` int(10) UNSIGNED NOT NULL,
+  `group_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `student_group`
+--
+
+INSERT INTO `student_group` (`student_id`, `group_id`) VALUES
+(1, 1),
+(3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `first_name` varchar(60) NOT NULL,
+  `last_name` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`id`, `email`, `password`, `first_name`, `last_name`) VALUES
+(6, 'damian@example2.com', '1234567Aa', 'Damian', 'Nowak'),
+(7, 'marian@kowalski.com', '$2a$10$YPRvp9BtfeK.DmMdlEeRJegN2SyQsx4Ei.Qcwfa9l44QtRA.jre5y', 'Marian', 'Kowalski'),
+(9, 'marek@example.com', 'ioie21ie21\'09sdpokl', 'Marek', 'Nowak');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `groups_teacherId_fk` (`teacher_id`) USING BTREE;
+
+--
+-- Indexes for table `invitation`
+--
+ALTER TABLE `invitation`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `invitation_groupId_fk` (`uid`) USING BTREE;
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_email_unique` (`email`) USING BTREE;
+
+--
+-- Indexes for table `student_group`
+--
+ALTER TABLE `student_group`
+  ADD PRIMARY KEY (`student_id`,`group_id`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_email_unique` (`email`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `Id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `teacher`
+--
+ALTER TABLE `teacher`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
